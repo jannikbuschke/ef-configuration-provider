7428c97e78a1e6c8eb9ebf17f24af632565c251d
Jannik Buschke <jannik.buschke@gmail.com>
2020-01-19 15:41:04 +0100

    try fix ci error

--- ui/Extensions.cs
+++ ui/Extensions.cs
@@ -20,15 +20,6 @@ namespace EfConfigurationProvider.Ui
 
         public static void UseEfConfigurationUi(this IApplicationBuilder app)
         {
-            IHostingEnvironment env = app.ApplicationServices.GetService<IHostingEnvironment>();
-            //app.UseSpaStaticFiles();
-            //app.UseDirectoryBrowser(new DirectoryBrowserOptions
-            //{
-            //    RequestPath = "/__configuration",
-            //    FileProvider = new ManifestEmbeddedFileProvider(
-            //        assembly: Assembly.GetAssembly(typeof(Extensions)), "web/build")
-            //});
-
             app.UseStaticFiles(new StaticFileOptions
             {
                 RequestPath = "/__configuration",
@@ -36,15 +27,6 @@ namespace EfConfigurationProvider.Ui
                 FileProvider = new ManifestEmbeddedFileProvider(
                     assembly: Assembly.GetAssembly(typeof(Extensions)), "web/build")
             });
-
-            if (env.IsDevelopment())
-            {
-                app.UseSpa(spa =>
-                {
-                    spa.Options.SourcePath = "web";
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
-                });
-            }
         }
     }
 }
