e5b47038f413c3fa640bc302f6dae857ef1df661
Jannik Buschke <jannik.buschke@gmail.com>
2020-01-23 10:27:23 +0100

    format

--- ui/Extensions.cs
+++ ui/Extensions.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 
